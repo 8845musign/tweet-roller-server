f2ae2bcca5320c7d331c50d4affe70ad3a22d8d0
8845musign <8845musign@gmail.com>
2017-10-01 14:49:28 +0900

    imple pomp

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const app = require('express')()
 const http = require('http').Server(app)
+const io = require('socket.io')(http)
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('hoge')
+})
+
+app.get('/pomp', (req, res) => {
+  console.log('pomp')
+  io.emit('pomp')
+  res.send('pomp')
+})
+
+io.on('connection', _ => {
+  console.log('a user connected')
 })
 
 http.listen(PORT, () => {
